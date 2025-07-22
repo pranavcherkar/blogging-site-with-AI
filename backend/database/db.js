6a0d63b06eb4b4a643b1171ac9ebca99dffc7525
Pranav Cherkar <pranavcherkar.2002@gmail.com>
2025-07-23 02:11:11 +0530

    Testing

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    // await mongoose.connect(`mongodb://localhost:27017/Users`);
     console.log("Mongodb Data connnected");
   } catch (error) {
     console.log("MongoDB connection error", error);
