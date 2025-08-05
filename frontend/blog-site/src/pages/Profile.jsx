e79d137143bbc5061e4db81f0f7b6849bfcbb365
Pranav Cherkar <pranavcherkar.2002@gmail.com>
2025-08-05 22:38:14 +0530

    Profile compoment part 1

--- frontend/blog-site/src/pages/Profile.jsx
+++ frontend/blog-site/src/pages/Profile.jsx
@@ -1,7 +1,60 @@
+import { Avatar, AvatarImage } from "../components/ui/avatar";
+import { Card } from "../components/ui/card";
 import React from "react";
-
+import userLogo from "../assets/user.jpg";
+import { Link } from "react-router-dom";
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { Label } from "../components/ui/label";
+import { Button } from "../components/ui/button";
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="pt-20 md:ml-[320px] md:h-screen">
+      <div className="max-w-6xl mx-auto mt-8">
+        <Card className="flex md:flex-row flex-col gap-10 p-6 md:pd-10 dark:bg-gray-800 mx-4 md:mx-0">
+          {/* image section */}
+          <div className="flex flex-col items-center justify-center md:w-[400px]">
+            <Avatar className="w-40 h-40 border-2">
+              <AvatarImage src={userLogo} />
+            </Avatar>
+            <h1 className="text-center font-semibold text-xl text-gray-700 dark:text-gray-300 my-3">
+              Mern Stack Developer
+            </h1>
+            <div className="flex gap-4 items-center">
+              <Link>
+                <FaFacebook className="w-6 h-6 text-gray-800 dark:text-gray-300" />
+              </Link>
+              <Link>
+                <FaLinkedin className="w-6 h-6 text-gray-800 dark:text-gray-300" />
+              </Link>
+              <Link>
+                <FaGithub className="w-6 h-6 text-gray-800 dark:text-gray-300" />
+              </Link>
+              <Link>
+                <FaInstagram className="w-6 h-6 text-gray-800 dark:text-gray-300" />
+              </Link>
+            </div>
+          </div>
+          {/* info section  */}
+          <div>
+            <h1 className="font-bold text-center md:text-start text-4xl mb-7">
+              Welcome User !
+            </h1>
+            <p className=" font-semibold">
+              <span>Email :</span>abc@abc.com
+            </p>
+            <div className="flex flex-col gap-2 items-start justify-start my-5">
+              <Label>About Me</Label>
+              <p className="border dark:border-gray-600 p-6 rounded-lg">
+                Lorem sdsdf sdsdfdfsdfsdf sdsdfdgfgfgdfgdf
+                dfgdfgdgdfgdfgdfgdfgdgdfgdgdfgdfgdfgdfgdfgfdgdfgdfgdfgdfg
+              </p>
+            </div>
+            <Button>Edit Profile</Button>
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
