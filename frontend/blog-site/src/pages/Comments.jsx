5ed6379d60b68c1eb666711883914fe4f245189a
Pranav Cherkar <pranavcherkar.2002@gmail.com>
2025-08-16 21:01:18 +0530

    all comments fetched to dashboard

--- frontend/blog-site/src/pages/Comments.jsx
+++ frontend/blog-site/src/pages/Comments.jsx
@@ -1,7 +1,73 @@
-import React from "react";
-
+import { Card } from "../components/ui/card";
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../components/ui/table";
+import { useNavigate } from "react-router-dom";
+import { Eye } from "lucide-react";
+import axios from "axios";
 const Comments = () => {
-  return <div>Comments</div>;
+  const [allComments, setAllComments] = useState([]);
+  const navigate = useNavigate();
+  const getTotalComments = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:8086/api/v1/comment/my-blogs/comments`,
+        { withCredentials: true }
+      );
+      if (res.data.success) {
+        setAllComments(res.data.comments);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getTotalComments();
+  }, []);
+  return (
+    <div className="pb-10 pt-20 md:ml-[320px] h-screen">
+      <div className="max-w-6xl mx-auto mt-8 ">
+        <Card className="w-full p-5 space-y-2 dark:bg-gray-800">
+          <Table>
+            <TableCaption>A list of your recent Comments.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Blog Title</TableHead>
+                <TableHead>Comment</TableHead>
+                <TableHead>Author</TableHead>
+                <TableHead className="text-center">Action</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {allComments.map((comment, index) => (
+                <TableRow key={index}>
+                  <TableCell className="font-medium">
+                    {comment.postId.title}
+                  </TableCell>
+                  <TableCell>{comment.content}</TableCell>
+                  <TableCell>{comment.userId.firstname}</TableCell>
+                  <TableCell className="text-right flex gap-3 items-center justify-center">
+                    <Eye
+                      className="cursor-pointer"
+                      onClick={() => navigate(`/blogs/${comment.postId._id}`)}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Comments;
