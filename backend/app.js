740ac7f28446e5227fa03f2c0a6b80f63da89f78
Pranav Cherkar <pranavcherkar.2002@gmail.com>
2025-08-30 17:58:05 +0530

    containerized the project

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import blogRoute from "./routes/blog.routes.js";
 import commentRoute from "./routes/comment.routes.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
+// import path from "path";
 const app = express();
 dotenv.config();
 app.use(express.json());
@@ -14,25 +14,26 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: /http:\/\/localhost(:5173|:80)?/,
     credentials: true,
   })
 );
-const __dirname = onratechange.resolve();
+
+// const __dirname = onratechange.resolve();
 ///apis
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/blog", blogRoute);
 app.use("/api/v1/comment", commentRoute);
 
 // Serve static files from the correct path
-app.use(express.static(path.join(__dirname, "../frontend/blog-site/dist")));
+// app.use(express.static(path.join(__dirname, "../frontend/blog-site/dist")));
 
-// Fallback route for SPA
-app.get("*", (_, res) => {
-  res.sendFile(
-    path.resolve(__dirname, "../frontend/blog-site/dist", "index.html")
-  );
-});
+// // Fallback route for SPA
+// app.get("*", (_, res) => {
+//   res.sendFile(
+//     path.resolve(__dirname, "../frontend/blog-site/dist", "index.html")
+//   );
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
